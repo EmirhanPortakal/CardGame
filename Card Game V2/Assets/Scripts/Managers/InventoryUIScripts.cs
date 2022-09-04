3b7e67ff039b65164619a706780b750f746d9555
harunfe0 <103264244+harunfe0@users.noreply.github.com>
2022-09-05 01:49:11 +0300

    deneme

---
+++ Card Game V2/Assets/Scripts/Managers/InventoryUIScripts.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class InventoryUIScripts : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+   
+   public GameObject InventoryPanel;
+   private void Awake()
+   {
+      InventoryPanel.gameObject.SetActive(false);
+
+   }
+
+    public void OnPointerEnter(PointerEventData eventData)
+
+    {
+      Debug.Log("mouse girdi");
+      InventoryPanel.gameObject.SetActive(true);
+
+    }
+
+   public void OnMouseOver(PointerEventData eventData)
+   {
+    
+    //InventoryPanel.gameObject.SetActive(true);
+
+   }
+
+   public void OnPointerExit(PointerEventData eventData)
+   {
+    Debug.Log("mouse cikti");
+    // InventoryPanel.gameObject.SetActive(false);
+    
+     
+}
+public void OnPointerDown(PointerEventData eventData){}
+public void OnPointerUp(PointerEventData eventData){}
+private void AnalyzePointerUp(PointerEventData eventData){}
+public void OnDrag(PointerEventData eventData){}
+
+
+}
